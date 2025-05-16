0aa9abd2e838af7edba0a94c374cc79d8f439a8d
Bogdan Zinovij <bogdanolexandrov@gmail.com>
2025-05-16 15:13:27 +0300

    chore: add database module and PostgreSQL Docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WeatherModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, WeatherModule],
 })
 export class AppModule {}
