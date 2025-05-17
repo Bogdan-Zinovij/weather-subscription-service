88dce43948ebde306327c75d2a2d4617d848a38b
Bogdan Zinovij <bogdanolexandrov@gmail.com>
2025-05-17 17:49:30 +0300

    feat: add subscription html page

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
 import { DatabaseModule } from './database/database.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ConfigModule.forRoot(),
     DatabaseModule,
     WeatherModule,
